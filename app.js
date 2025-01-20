a17cc4761fec5871e2836c2d9bf875f512f77e1d
Denny Rubino <rubidenny@gmail.com>
2025-01-20 13:56:50 +0100

    Add img and express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("ciao")
+const express = require('express')
+const app = express()
+const port = 3000   
+
+app.get('/', (req, res) => {
+  res.send('Server del mio blog')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
