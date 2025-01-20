e2cb55fd016443311b53cdcba98a37094c094977
Denny Rubino <rubidenny@gmail.com>
2025-01-20 14:14:27 +0100

    fix img

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
 
+app.use(express.static("public"))
+
+app.get('/bacheca', (req, res) => {
+    res.json([
+        {
+          name: 'Ciambellone',
+          image: `http://localhost:3000/cibi/ciambellone.jpeg`,
+          
+        },
+        {
+          name: "cracker barbabietola",
+          image: `http://localhost:3000/cibi/cracker_barbabietola.jpeg`,
+         
+        },
+        {
+          name: "pane fritto dolce",
+          image: `http://localhost:3000/cibi/pane_fritto_dolce.jpeg`,
+          
+        },
+        {
+          name: "pasta barbabietola",
+          image: `http://localhost:3000/cibi/pasta_barbabietola.jpeg`,
+         
+        },
+        {
+          name: "torta paesana",
+          image: `http://localhost:3000/cibi/torta_paesana.jpeg`,
+          
+        }
+    ])
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
